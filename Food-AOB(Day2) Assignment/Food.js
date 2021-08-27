db061ca028174b25a0b07f7944e3511f19ed6065
Shubham Goel <75998290+shubhamgoelgithub@users.noreply.github.com>
2021-08-27 12:54:19 +0530

    Day 2 Assignment Submitted

---
+++ Food-AOB(Day2) Assignment/Food.js
@@ -0,0 +1,9 @@
+let food = [{ Name: "Pizza", Price: 300, Quantity: 5 },
+{ Name: "Dosa", Price: 500, Quantity: 7 },
+{ Name: "Momos", Price: 180, Quantity: 10 },
+{ Name: "Burger", Price: 280, Quantity: 20 }
+];
+
+for (let num = 0; num < food.length; num++) {
+    console.log(food[num]);
+}
